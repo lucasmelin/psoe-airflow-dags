04c875bd3b6e08f7f28621e72d52fe2c1a9333ec
Lucas Melin <lucasmelin@users.noreply.github.com>
2020-03-31 15:59:47 -0400

    Add concatenator dags

--- download_cve_data.py
+++ download_cve_data.py
@@ -11,6 +11,7 @@ from azure.storage.blob import BlockBlobService, PublicAccess
 from airflow.hooks.base_hook import BaseHook
 import logging
 
+
 def get_cve_json(vendor, product):
     r = requests.get(f'http://cve.circl.lu/api/search/{vendor}/{product}')
     return r.json()
@@ -69,3 +70,41 @@ with DAG('export_cve_data', description='Export CVE data', schedule_interval='@o
     export_task = PythonOperator(task_id='export_task', python_callable=get_product_cves)
 
 export_task
+
+
+def create_dag(dag_id,
+               schedule,
+               dag_number,
+               default_args):
+
+    def concatenate_values(*args):
+        lst = []
+        for i in range(1000000):
+            lst.append('x')
+
+    dag = DAG(dag_id,
+              schedule_interval=schedule,
+              default_args=default_args)
+
+    with dag:
+        t1 = PythonOperator(
+            task_id='concatenator',
+            python_callable=concatenate_values,
+            dag_number=dag_number)
+
+    return dag
+
+
+# build a dag for each number in range(10)
+for n in range(1, 10):
+    dag_id = 'concatenator_{}'.format(str(n))
+
+    default_args = {'owner': 'airflow',
+                    'start_date': datetime(2020, 1, 1)
+                    }
+    schedule = '@daily'
+    dag_number = n
+    globals()[dag_id] = create_dag(dag_id,
+                                  schedule,
+                                  dag_number,
+                                  default_args)
